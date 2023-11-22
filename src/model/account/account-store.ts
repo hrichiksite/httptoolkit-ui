a3b3bf9ff6d12dee6f197f2e95a21cf88da8d91e
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2023-11-22 10:39:59 +0530

    Free
    sorry

--- src/model/account/account-store.ts
+++ src/model/account/account-store.ts
@@ -94,9 +94,7 @@ export class AccountStore {
     }
 
     @computed get userSubscription() {
-        return this.isPaidUser || this.isPastDueUser
-            ? this.user.subscription
-            : undefined;
+        return true;
     }
 
     private updateUser = flow(function * (this: AccountStore) {
